bf1729437d3d7ba4924a38c0151111f07be10404
Dheeraj <dheeraj2032006@gmail.com>
2025-10-07 12:38:34 +0530

    added few pages

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
+<<<<<<< HEAD
   baseURL: "http://localhost:8000/api",  //use this to just use pre declared url instead of manually typing it everytime. (axios.get("http://localhost:8000/api"))
+=======
+  baseURL: "http://localhost:8000/api/",  //use this to just use pre declared url instead of manually typing it everytime. (axios.get("http://localhost:8000/api"))
+>>>>>>> 5e56ae118495c6c870332730c70e004d8619c4e0
 })
 
 export default API;
